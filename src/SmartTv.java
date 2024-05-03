399ef676c0becec82565581f978a27a333dffd1c
Andre Prohmann <98752890+andreprohmann@users.noreply.github.com>
2024-05-03 16:24:56 -0300

    Finish

---
+++ src/SmartTv.java
@@ -0,0 +1,35 @@
+public class SmartTv {
+    boolean ligada = false;
+    boolean desligada = true;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal (){
+        canal++;
+    }
+    public void diminuirCanal (){
+        canal--;
+    }
+
+    public void aumentarVolume (){
+        volume ++;
+        System.out.println("Aumentando o Volume para: " + volume);
+        //volume = volume + 1;
+    }
+
+    public void diminuirVolume (){
+        //volume = volume - 1;
+        volume --;
+        System.out.println("Diminuir o Volume para: " + volume);
+    }
+
+    public void ligar () {
+        ligada = true;
+    }
+    public void desligar () {
+        desligada = true;
+    }
+}
